77dc0c225b35c5bc643d0f94c6d08064b2d4a47e
soheesgit <thgml89567@smail.kognju.ac.kr>
2026-08-25 17:40:37 +0900

    feat: 회원가입 기능 추가

--- auth/routes.py
+++ auth/routes.py
@@ -1,6 +1,53 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, request, url_for
+from werkzeug.security import generate_password_hash
+
+from db import db
+
 auth_bp = Blueprint('auth', __name__)
+users = db["users"]
 
 @auth_bp.route('/login')
 def login():
-    return "login page"
+    return render_template("auth/login.html")
+
+@auth_bp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        userid = request.form.get("userid")
+        nickname = request.form.get("nickname")
+        password = request.form.get("password")
+        repassword = request.form.get("repassword")
+
+        # 모두 입력 안 했을 경우
+        if not all([userid, nickname, password, repassword]):
+            return render_template(
+                "auth/signup.html",
+                error="모든 항목을 입력해 주세요.")
+        if users.find_one({"userid": userid}):
+            return render_template(
+                "auth/signup.html",
+                error="이미 사용중인 아이디입니다.")
+        
+        #비밀번호가 일치하지 않는 경우
+        if (password != repassword):
+            return render_template(
+                "auth/signup.html",
+                error="비밀번호가 일치하지 않습니다.")
+        if (len(password) < 8):
+            return render_template(
+                "auth/signup.html",
+                error="비밀번호는 8자 이상이여야 합니다."
+            )
+        password_hash = generate_password_hash(password)
+
+        user = {
+            "userid": userid,
+            "nickname": nickname,
+            "password": password_hash
+        }
+
+        users.insert_one(user)
+
+        return redirect(url_for("auth.login"))
+    
+    return render_template("auth/signup.html")
