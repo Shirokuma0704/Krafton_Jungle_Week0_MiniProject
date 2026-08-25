012d7356a85107ecff2c76b204238179752d7f4f
Beak <a34105451@gmail.com>
2026-08-25 21:59:12 +0900

    Set up initial project structure with Flask blueprints, MongoDB, and environment configuration

--- ideas/routes.py
+++ ideas/routes.py
@@ -1,7 +1,103 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, request, redirect
+from db import db
+from bson import objectid
+from datetime import datetime, timezone
+
 
 idea_bp = Blueprint('idea', __name__)
 
-@idea_bp.route
-def idea():
-    return "idea page"
+def is_member(team_id, user_id):
+    check = db.team_members.find_one({"team_id": team_id, "user_id": user_id})
+    if check is None:
+        return False
+    return True
+
+# def validate_user_team(user_id, team_id): 시간 남으면
+
+@idea_bp.route('/dev/login/<user_id>')
+def dev_login(user_id):
+    session['user_id'] = user_id
+    return f'로그인됨: {user_id}'
+
+@idea_bp.route('/idea/<team_id>')
+def idea(team_id):
+    user_id = session.get('user_id')
+    if user_id is None:
+        return "401"
+
+    user_id = objectid.ObjectId(user_id)
+    team_id = objectid.ObjectId(team_id)
+    member_check = is_member(team_id,user_id)
+    if not member_check:
+        return "403"
+
+    docs = list(db.ideas.find({"team_id": team_id}))
+    return render_template('team/idea.html', ideas=docs, user_id= user_id)
+
+@idea_bp.route('/idea/<team_id>/add', methods=['POST'])
+def add_idea(team_id):
+    user_id = session.get('user_id')
+    if user_id is None:
+        return "401"
+
+    user_id = objectid.ObjectId(user_id)
+    team_id = objectid.ObjectId(team_id)
+    member_check = is_member(team_id, user_id)
+    if not member_check:
+        return "403"
+
+    title = request.form['title']
+    content = request.form['content']
+    db.ideas.insert({"team_id": team_id, "author_id":user_id, "title":title, "content":content, "created_at":datetime.now(timezone.utc)})
+    return redirect(f'/idea/{team_id}')
+
+@idea_bp.route('/idea/<idea_id>/delete', methods=['POST'])
+def del_idea(idea_id):
+
+    user_id = session.get('user_id')
+    if user_id is None:
+        return "401"
+    user_id = objectid.ObjectId(user_id)
+
+    idea_id = objectid.ObjectId(idea_id)
+    idea_data = db.ideas.find_one({"_id": idea_id})
+    if idea_data is None:
+        return "404"
+
+    if user_id != idea_data["author_id"]:
+        return "403"
+
+    team_id = idea_data["team_id"]
+    member_check = is_member(team_id, user_id)
+    if not member_check:
+        return "403"
+
+
+    db.ideas.delete_one({"_id": idea_id})
+    return redirect(f'/idea/{team_id}')
+
+@idea_bp.route('/idea/<idea_id>/edit', methods=['POST'])
+def edit_idea(idea_id):
+
+    user_id = session.get('user_id')
+    if user_id is None:
+        return "401"
+    user_id = objectid.ObjectId(user_id)
+
+    idea_id = objectid.ObjectId(idea_id)
+    idea_data = db.ideas.find_one({"_id": idea_id})
+    if idea_data is None:
+        return "404"
+
+    if user_id != idea_data["author_id"]:
+        return "403"
+
+    team_id = idea_data["team_id"]
+    member_check = is_member(team_id, user_id)
+    if not member_check:
+        return "403"
+    title = request.form['title']
+    content = request.form['content']
+    db.ideas.update_one({"_id": idea_id}, {"$set": {"title":title, "content":content}})
+    return redirect(f'/idea/{team_id}')
+
