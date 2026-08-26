888b0363808818d4f6d52450e573205ea16660b8
soheesgit <thgml89567@smail.kognju.ac.kr>
2026-08-26 13:46:59 +0900

    feat: 메인 페이지-방 상태 조회 기능 구현

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template
 from auth.routes import auth_bp
-from teams.routes import teams_bp
+from teams.routes import teams_bp, get_teams
 
 load_dotenv()
 
@@ -14,7 +14,13 @@ app.register_blueprint(teams_bp, url_prefix="/teams")
 
 @app.route('/')
 def index():
-    return render_template("main/index.html")
+    process_team, done_team = get_teams()
+
+    return render_template(
+        "main/index.html",
+        process_team=process_team,
+        done_team=done_team
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
